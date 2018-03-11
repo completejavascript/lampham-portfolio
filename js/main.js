257c3a4d82ed76c3e88ffb0590022dc9e130aa89
completejavascript <completejavascript.super@gmail.com>
2018-03-11 18:03:11 +0700

    add sidebar

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#my-toggle-menu").click(function(){
+    $("#my-side-bar").toggleClass("responsive");
+    $("#my-toggle-menu").toggleClass("responsive");
+  });
+});
