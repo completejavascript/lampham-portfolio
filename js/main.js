b1652c25d07fdf828730b34733b499196fbf44e9
completejavascript <completejavascript.super@gmail.com>
2018-09-27 21:37:38 +0700

    apply lazy loading image

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function () {
   let portfolioTemplate = $('#portfolio-template').html();
   Mustache.parse(portfolioTemplate);
 
-  const numPortfolioInit = 6, numPortfolioLoad = 4; 
+  const numPortfolioInit = 6, numPortfolioLoad = 4;
   let portfolios;
 
   // Get data
@@ -12,12 +12,12 @@ $(document).ready(function(){
   });
 
   // Render more portfolio after clicking this button
-  $("#btn-see-more").click(function(){
+  $("#btn-see-more").click(function () {
     addPortfolio(numPortfolioLoad);
   });
 
   // Display side bar menu when clicking on toggle menu
-  $("#my-toggle-menu, .nav-item").click(function(){
+  $("#my-toggle-menu, .nav-item").click(function () {
     $("#my-side-bar").toggleClass("responsive");
     $("#my-toggle-menu").toggleClass("responsive");
   });
@@ -34,7 +34,7 @@ $(document).ready(function(){
 
   // Scroll to top as "intro" section
   let $myScrollTop = $("#my-scroll-top");
-  $myScrollTop.click(function(){
+  $myScrollTop.click(function () {
     scrollTo($("#intro"));
   });
 
@@ -42,19 +42,33 @@ $(document).ready(function(){
   showOrHideBackToTopButton();
 
   let debounceTimer;
-  $(window).scroll(function() {
-    if(debounceTimer) {
+  $(window).scroll(function () {
+    if (debounceTimer) {
       window.clearTimeout(debounceTimer);
     }
-  
-    debounceTimer = window.setTimeout(function() {
+
+    debounceTimer = window.setTimeout(function () {
       showOrHideBackToTopButton();
+      handleScrollLazyLoading();
     }, 100);
   });
 
   function showOrHideBackToTopButton() {
     if ($(window).scrollTop() > 150) $myScrollTop.show();
     else $myScrollTop.hide();
+  }
+
+  // Load profile image 
+  lazyLoadingImage(document.querySelector(".img-profile"));
+
+  function lazyLoadingImage(imgElm) {
+    const dataSrc = imgElm.getAttribute("data-src");
+    const img = new Image();
+    img.src = dataSrc;
+    img.onload = () => {
+      imgElm.setAttribute("src", dataSrc);
+      imgElm.classList.add("visible");
+    }
   }
 
   // Scroll to target
@@ -65,16 +79,37 @@ $(document).ready(function(){
   }
 
   function addPortfolio(numPortfolio) {
-    let rendered;
     for (let i = 0; i < numPortfolio; i++) {
-      let item = portfolios.pop();
-      if(item) {
-        rendered = Mustache.render(portfolioTemplate, item);
+      const item = portfolios.pop();
+      if (item) {
+        const rendered = Mustache.render(portfolioTemplate, item);
         $("#my-portfolios").append(rendered);
       }
     }
 
-    if (portfolios.length === 0) 
+    handleScrollLazyLoading();
+
+    if (portfolios.length === 0)
       $("#btn-see-more").css("display", "none");
+  }
+
+  function handleScrollLazyLoading() {
+    const imgElms = $("#my-portfolios").find(".lazy-image").not(".visible");
+    for (let i = 0; i < imgElms.length; i++) {
+      if (elementInViewport(imgElms[i])) {
+        lazyLoadingImage(imgElms[i]);
+      }
+    }
+  }
+
+  // Check if a DOM element is in the viewport
+  function elementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0
+      && rect.left >= 0
+      && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+    )
   }
 });
